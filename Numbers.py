9ccd33caad65c6663cb16aaf2cd415ecead7c11e
Sudarshan H R <sudarshanramakrishna82@gmail.com>
2020-11-28 23:58:56 +0530

    Added 1st 10 Classes

---
+++ Numbers.py
@@ -0,0 +1,64 @@
+import os
+os.system('clear')
+num = 10
+print(num)
+
+#use float
+
+num = 10
+print(float(num))
+
+#use int 
+
+num = 10.25
+print(int(num))
+
+#direct add 
+
+print(7+2)
+
+#direct subtraction
+
+print(7-2)
+
+#direct multiplication
+
+print(7*2)
+
+#direct division
+
+print(10/2)
+
+print(10/3)
+
+# direct  power 
+print(5**2)
+print(5**3)
+
+# direct modulus
+
+print(10%2)
+print(10%3)
+
+num_1 = 6
+num_2 = 5
+print(num_1 * num_2)
+
+#math operations 
+
+print(4 + 5 * 2)
+print((4 + 5) * 2)
+
+#conversion
+
+num = "5"
+print((int(num)) * 3)
+num = "10"
+print((int(num)) * 3)
+
+#if not converted then its gonna the samething 
+num = "5"
+print(num * 3) 
+
+num = "BA"
+print(num * 3) 
--- class7.py
+++ class7.py
@@ -4,3 +4,9 @@ os.system('clear')
  # this class we studied how to create github account 
  # how to add our pgns to the git hub 
  
+ '''
+ 3 steps to add files to git hub 
+ 1. git add .
+ 2. git commit -am " your wish"
+ 3. git push
+ '''
